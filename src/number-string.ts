c871236975a3042b6f93c43721b31c05615d653f
rue-lepic <nunemoga@gmail.com>
2022-02-06 16:11:57 +0900

    mod: number-string.ts

--- src/number-string.ts
+++ src/number-string.ts
@@ -1,6 +1,6 @@
 export {};
 
-let year: number = 1976;
+let year: number = 1986;
 // year = false;
 console.log(year);
 
@@ -8,7 +8,7 @@ let age: number = 0x2b;
 
 console.log(age);
 
-let name: string = 'Ham';
+let name: string = 'rue-lepic';
 // name = 10;
 console.log(name);
 
