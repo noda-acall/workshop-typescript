21ad3d0e28e773752dfe2c70254e30a93e5d71e2
rue-lepic <kosuke.noda@corp.acall.jp>
2022-06-04 11:31:18 +0100

    2022/06/04 commit

---
+++ src/enum.ts
@@ -0,0 +1,49 @@
+export {};
+
+enum Months {
+  January = 1,
+  February,
+  March,
+  April,
+  May,
+  June,
+  July,
+  August,
+  September,
+  October,
+  November,
+  Decenber
+}
+
+console.log(Months.January);
+console.log(Months.February);
+console.log(Months.Decenber);
+
+/* 
+const MonthsJs = {
+  January: 0,
+  February: 1
+};
+
+console.log(MonthsJs.January);
+console.log(MonthsJs.February); */
+
+enum COLORS {
+  RED = '#FF0000',
+  WHITE ='#FFFFFF',
+  GREEN = '#008000',
+  BLUE = '#0000FF',
+  // YELLOW = '#FFFF00',
+  BLACK = '#000000'
+};
+
+let green = COLORS.GREEN;
+console.log({ green} );
+
+enum COLORS {
+  YELLOW = '#FFFF00',
+  GRAY = '#808080'
+}
+
+COLORS.YELLOW;
+COLORS.GRAY;
